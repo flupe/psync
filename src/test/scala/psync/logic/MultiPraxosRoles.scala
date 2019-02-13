dded68af512c075f98c73ab6291b50a361da245d
flupe <lucas@escot.me>
2019-02-13 16:40:04 +0100

    added transition for round 2 of MPR

--- src/test/scala/psync/logic/MultiPraxosRoles.scala
+++ src/test/scala/psync/logic/MultiPraxosRoles.scala
@@ -35,8 +35,12 @@ class MultiPraxosRoles extends FunSuite {
   val payloadType   = UnInterpreted("payload")
 
   val send = UnInterpretedFct("send", Some(pid ~> FMap(pid, commandType)))
-  // val send = UnInterpretedFct("send", Some(pid ~> Product(payloadType, FSet(pid))))
   val mbox = UnInterpretedFct("mbox", Some(pid ~> FMap(pid, commandType)))
+
+  // right now the type of send is too permissive
+  // perhaps we could consider the following type
+  // val send = UnInterpretedFct("send", Some(pid ~> Product(payloadType, FSet(pid))))
+  // (i.e we can only send one msg at each roung -- to many processes)
 
   val axioms = And(
     // π is the set of all processes
@@ -57,10 +61,11 @@ class MultiPraxosRoles extends FunSuite {
   // invariant pre-broadcast
   val inv0 = And(
     leader ∈ act,
-    ForAll(List(p), lastIndex(p) ≡ lastIndex(leader))
+    // ForAll(List(p), lastIndex(p) ≡ lastIndex(leader))
   )
 
 
+  // =================================================================================
   // Inner algorithm, round 1
   val round1 = And(
     act1 ⊆ act,
@@ -71,10 +76,7 @@ class MultiPraxosRoles extends FunSuite {
     ForAll(List (p), (p ≠ leader) ==> (Size(send(p)) ≡ 0)),
 
     // leader sends current cmd
-    ForAll(List (p), (p ≡ leader) ==>
-      ForAll(List(q),
-        send(p).lookUp(q) ≡ log(p).lookUp(lastIndex(p))._1)
-    ),
+    ForAll(List(q), send(leader).lookUp(q) ≡ log(leader).lookUp(lastIndex(leader))._1),
 
 
     // UPDATE ======================
@@ -90,26 +92,35 @@ class MultiPraxosRoles extends FunSuite {
     // update log and stay active
     ForAll(List (p),
       ((p ∈ act) ∧ (Size(mbox(p)) > 0)) ==>
-        (Exists(List(q),
-          log1(p) ≡ log(p).updated(lastIndex(p), Tuple(mbox(p).lookUp(q), False()))
-        ) ∧ (p ∈ act1))
+          ((log1(p) ≡
+              log(p).updated(lastIndex(p), Tuple((log(leader).lookUp(lastIndex(leader)))._1, False())))
+        ∧ (p ∈ act1))
     ),
 
     // otherwise, becomes inactive, and conserves values
     ForAll(List (p), (p ∉ act) ==> ((p ∉ act1) ∧ (log1(p) ≡ log(p)))),
+
     ForAll(List (p), ((p ∈ act) ∧ (Size(mbox(p)) ≡ 0)) ==>
         ((p ∉ act1) ∧ (log1(p) ≡ log(p)))),
 
   )
 
-  // invariant post-round1
+  /* Invariant Post-Round1
+   * - leader is active
+   * - if node is active, it has the same active command as the leader, uncommitted
+   * - the previous log is left untouched
+   */
   val inv1 = And(
+    // leader active
+    leader ∈ act1,
+
+    // all active processes have same command
     ForAll(List(p),
       (p ∈ act1) ==> (log1(p).lookUp(lastIndex1(p))._1 ≡ log(leader).lookUp(lastIndex(leader))._1)
     )
   )
 
-  test("inv0 ∧ round1 ⇒ inv1") {
+  ignore("inv0 ∧ round1 ⇒ inv1") {
     val fs = List(
       axioms,
       round1,
@@ -119,5 +130,55 @@ class MultiPraxosRoles extends FunSuite {
     assertUnsat(fs, to=60000, reducer=c2e1)
     // getModel(fs, reducer=c2e1)
   }
+
+  // =================================================================================
+  // Inner algorithm, round 2
+  val send2 = UnInterpretedFct("send", Some(pid ~> FMap(pid, UnitT())))
+  val mbox2 = UnInterpretedFct("mbox", Some(pid ~> FMap(pid, UnitT())))
+
+  val round2 = And(
+    act1 ⊆ act,
+
+    // SEND ========================
+
+    // leader do not send anything
+    Size(send2(leader)) ≡ 0,
+
+    // non-leaders send receipt to leader
+    ForAll(List(p), ((p ∈ act) ∧ (p ≠ leader)) ==> send2(p).lookUp(leader) ≡ UnitLit()),
+
+    // non-active nodes do nothing
+    ForAll(List(p), (p ∉ act) ==> Size()),
+
+
+    // UPDATE ======================
+
+    // active processes that aren't the leader stay active
+    ForAll(List(p), ((p ∈ act) ∧ (p ≠ leader)) ==> (p ∈ act1)),
+
+    // non-leaders keep same log
+    ForAll(List(p), (p ≠ leader) ==> (log1(p) ≡ log(p))),
+
+    // all nodes keep same lastIndex
+    ForAll(List(p), lastIndex1(p) ≡ lastIndex(p)),
+
+    // if leader receives majority, commits and stays active
+    (Size(mbox2(leader)) >= (n / 2)) ==> (
+      log(leader).updated(lastIndex(leader),
+        Tuple((log(leader).lookUp(lastIndex(leader)))._1, True()))
+      ∧ (leader ∈ act1)
+    ),
+
+    // otherwise, leader no longer active
+    // keeps same log
+    (Size(mbox(leader)) < (n / 2)) ==> ((log1(leader) ≡ log(leader)) ∧ (leader ∉ act1)),
+  )
+
+  /* Invariant Post-Round2
+   * - if leader is active, a majority of nodes have the same active command as the leader,
+   * - if leader is active, it has committed the value
+   * - other nodes have the same prefix-log (?)
+   */
+  val inv2 = And()
 
 }
